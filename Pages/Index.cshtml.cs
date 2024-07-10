e9f39680b9e42143ef79cd853f30dd8c4d528266
Jonathan Hurd <hurd.jonathan@gmail.com>
2024-07-09 21:51:37 -0500

    Implement program version fetching and display

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using CliWrap;
+using CliWrap.Buffered;
 
 namespace ProVision_Takehome.Pages;
-
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
+    public Dictionary<string, string> ProgramVersions { get; private set; }
+
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+        ProgramVersions = new Dictionary<string, string>();
     }
 
-    public void OnGet()
+    public async Task OnGet()
     {
+        ProgramVersions = await GetProgramVersions();
+    }
 
+    static async Task<Dictionary<string, string>> GetProgramVersions()
+    {
+        var programVersions = new Dictionary<string, string>();
+        var cliCommands = new Dictionary<string, string>
+        {
+            { "dotnet", "--version" },
+            {"npm", "--version"},
+            {"python", "--version"}
+            
+        };
+
+        foreach (var program in cliCommands)
+        {
+            var result = await Cli.Wrap(program.Key)
+                .WithArguments(program.Value)
+                .ExecuteBufferedAsync();
+
+            programVersions[program.Key] = result.StandardOutput;
+        }
+
+        return programVersions;
     }
 }
